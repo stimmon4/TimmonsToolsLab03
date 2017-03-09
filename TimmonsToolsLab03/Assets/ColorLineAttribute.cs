d2eadcb57195f03f9d0131a07c3c8499f789d851
svc-student <svc-student@student.directory.jccc>
2017-03-08 20:04:53 -0600

    Did the decorator drawer

---
+++ TimmonsToolsLab03/Assets/ColorLineAttribute.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ColorLineAttribute : PropertyAttribute
+{
+    public float lineHeight;
+    public float lineWidth;
+    public Color lineColor = Color.red;
+	
+	public ColorLineAttribute(float lineHeight, float lineWidth, float red, float green, float blue)
+    {
+        this.lineHeight = lineHeight;
+        this.lineWidth = lineWidth;
+
+        this.lineColor = new Color(red, green, blue);
+    }
+
+}
+[CustomPropertyDrawer(typeof(ColorLineAttribute))]
+public class ColorLineDrawer : DecoratorDrawer
+{
+    ColorLineAttribute colorLine
+    {
+        get { return ((ColorLineAttribute)attribute); }
+    }
+    public override float GetHeight()
+    {
+        return 10f;
+    }
+    public override void OnGUI(Rect position)
+    {
+        GUIStyle centerStyle = GUI.skin.GetStyle("Label");
+        centerStyle.alignment = TextAnchor.MiddleCenter;
+
+        float lineX = (position.x + (position.width / 2)) - colorLine.lineWidth / 2;
+        float lineY = position.y;
+        float lineWidth = colorLine.lineWidth;
+        float lineHeight = colorLine.lineHeight;
+
+        //Store the current color in the previous color variable, set GUI.color equal to whatever color is provided by the constructor, draw the line using the specified coordinates by 
+        //tinting the whiteTexture with the lineColor, and store the color as the previous color.
+        
+        Color prevColor = GUI.color;
+        GUI.color = colorLine.lineColor;
+        EditorGUI.DrawPreviewTexture(new Rect(lineX, lineY, lineWidth, lineHeight), Texture2D.whiteTexture);
+        GUI.color = prevColor;
+    }
+}
---
+++ TimmonsToolsLab03/Assets/ColoredLine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColoredLine : MonoBehaviour {
+    public string beginColor = "The line before the colored line";
+    [ColorLine(10, 320, 255, 0, 0)]
+    public string endColor = "The line after the colored line";
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
